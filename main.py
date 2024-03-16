5e6a100dbf16f7de233d14eaec06d97001d3559b
zhMargDev <155582703+zhMargDev@users.noreply.github.com>
2024-03-17 02:22:08 +0400

    a-2 commit

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI, Request, HTTPException, Depends, Header
+from fastapi import Cookie, FastAPI, Request, HTTPException, Depends, Header, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+# Main page
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("main.html", {"request": request})
+
+# Request to server for adding sessions so that the notification does not show anymore
+@app.post("/block_notification")
+async def block_notification(request: Request):
+    response = Response(status_code=200)  # Create a response object
+    response.set_cookie(
+        key="block_notification",  # Set the cookie with the specified key
+        value="true",  # Set its value to "true"
+        expires=365 * 24 * 60 * 60,  # Set the expiration time to one year
+        path="/",  # Make the cookie accessible from all paths on your domain
+    )
+    return response
---
+++ static/js/main.js
@@ -0,0 +1,39 @@
+function checking_cookie(){
+    // Get cookies
+    const cookies = document.cookie.split(';');
+
+    // Checking cookies
+    for (const cookie of cookies) {
+        const [key, value] = cookie.split('='); //Split cookie string
+        // check if cookie is block_notification = true
+        if(key === ' block_notification' && value === 'true'){
+            document.getElementById('popupPushBox').remove();
+        }
+    }
+}
+checking_cookie()
+
+async function close_popupAppeal(e){
+    e.parentElement.parentElement.parentElement.remove(); //Remove id #popupPushBox
+}
+
+async function cloeAndBlock_popupAppeal(e){
+    close_popupAppeal(e); //Remove id #popupPushBox
+
+    //Request to server for adding sessions so that the notification does not show anymore
+    const xhr = new XMLHttpRequest();
+    //Request to server
+    xhr.open("POST", "/block_notification");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = function() {
+        // If problem with server
+        if(xhr.status !== 200){
+            alert('Проблемы с сервером.')
+        }
+    };
+
+    xhr.send(JSON.stringify(true));
+
+}
+
